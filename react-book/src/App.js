a99a14483df136562ecefa0d258dd32f84ee5ae8
Uriyah Ann <uriyahruiz1@gmail.com>
2021-02-03 02:40:18 -0800

    Complete inline-handler

--- react-book/src/App.js
+++ react-book/src/App.js
@@ -1,62 +1,20 @@
-import React from "react";
+import React, { Children } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { Container, Row, Col } from "react-bootstrap";
-// import DataList from '../src/components/DataList'
-
-function getTitle(title) {
-  return "My " + title;
-}
-
-const useSemiPersistentState = (key, initialState) => {
-  /** useSemiPersistantState
-   *  - pass in a key in order to fix overwrite of the 'value' allocated in local storage
-   *    when custom hook is used more than once
-   *  - give custom hook initial state from outside
-   *  - manages state yet synchronizes with the local storage
-   *  - no fully consistent; clearing local storage browser deletes relevant data from application
-   */
-  const [value, setValue] = React.useState(
-    localStorage.getItem(key) || initialState
-  );
-  React.useEffect(() => {
-    localStorage.setItem(key, value);
-  }, [value, key]);
-  return [value, setValue];
-};
+import { Container, Row, Col } from "react-bootstrap";
+import DataList from "../src/components/DataList";
 
 // refactor: component-definition
 const App = () => {
-  /**
-   * before the return is standard javascript
-   * inside the return is HTML
-   * never use var
-   * let can before maluable
-   */
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  // const [searchTerm, setSearchTerm] = React.useState(
-  //   localStorage.getItem("search") || "React"
-  // );
-
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  };
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -69,18 +27,87 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1> {getTitle("Hacker Stories")}</h1>
+      <h1> My Hacker Stories</h1>
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong> Search : </strong>
+      </InputWithLabel>
 
-      <Search onSearch={handleSearch} />
+      {/* <Search onSearch={handleSearch} /> */}
       <hr />
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
 
       {/* <DataList list={list}></DataList> */}
     </div>
   );
 };
 
-const List = ({ list }) =>
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const getAsyncStories = () => 
+  Promise.resolve({ data: { stories: initialStories }});
+
+const InputWithLabel = ({
+  id,
+  label,
+  value,
+  type = "text",
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  // create ref with useRef hook; has persistent value stays intact over the lifetime of the component
+  // ref comes with property current; in contrast to ref, current is changeable
+  const inputRef = React.useRef();
+
+  // useEffect hook performing focuse on input field when component renders (or dependencies change)
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      // current changes and executes focus programmatically as side-effect
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}> {children} </label>
+      &nbsp;
+      {/* ref={inputRed}: assigned to changable current property */}
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
+
+const List = ({ list, onRemoveItem }) =>
   /**
    * spread operator: allows to spread all key/value pairs of an object to another object
    *  - instead of passing each property one at a time via props from List to Item component,
@@ -92,19 +119,38 @@ const List = ({ list }) =>
    *   - happens always as the last part of an object destructing; on the right side of an assignment
    *   - always used to seperate an object from some of its properties
    */
-  list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />);
+  list.map(item => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
 
-const Item = ({ title, url, author, num_comments, points }) => (
-  <div>
-    <span>
-      <a href={url}> {title} </a>
-    </span>
+  
+const Item = ({ item, onRemoveItem }) => {
+  // const handleRemoveItem = () => {
+  //   onRemoveItem(item);
+  // };
+  return (
+    <div>
+      <span>
+        <a href={item.url}> {item.title} </a>
+      </span>
 
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
-  </div>
-);
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        {/* inline handlers: allows to excute the function in JSX
+            Bind Method: onClick={onRemoveItem.bind(null,item)}
+            (do not use for complex functions) Wrapping Arrow Function: onClick={() => onRemoveItem(item)} 
+        */}
+        <button type="button" onClick={onRemoveItem.bind(null,item)}>
+          Dismiss
+        </button>
+      </span>
+    </div>
+  );
+};
+
+
 const Search = ({ search, onSearch }) => {
   /**
    * searchTerm: represents the current state
@@ -126,17 +172,38 @@ const Search = ({ search, onSearch }) => {
   };
 
   return (
-    <div>
+    <>
+      {/* React Fragment: wraps other elements into a single top-level element without adding to rendered output */}
       {/* Synthetic Event: using browsers developer tools see loggin occur after you type into the input field */}
       {/* do not onChange={ handleChange() } */}
 
-      <label htmlFor="search"> Search: </label>
+      <label key="1" htmlFor="search">
+        {" "}
+        Search:{" "}
+      </label>
       <input id="search" type="text" value={search} onChange={onSearch} />
       <p>
         Searching for <strong>{searchTerm}</strong>
       </p>
-    </div>
+    </>
   );
+};
+
+const useSemiPersistentState = (key, initialState) => {
+  /** useSemiPersistantState: custom hook
+   *  - pass in a key in order to fix overwrite of the 'value' allocated in local storage
+   *    when custom hook is used more than once
+   *  - give custom hook initial state from outside
+   *  - manages state yet synchronizes with the local storage
+   *  - no fully consistent; clearing local storage browser deletes relevant data from application
+   */
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+  return [value, setValue];
 };
 
 class Developer {
